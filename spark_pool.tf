8d062b35d879f2c0bc9f7aa0024be5143d66d1e4
Roberth Strand <roberth.strand@crayon.com>
2022-09-28 16:42:36 +0200

    feat(spark-pool): added dynamic executor allocation
    Signed-off-by: Roberth Strand <roberth.strand@crayon.com>

--- spark_pool.tf
+++ spark_pool.tf
@@ -3,13 +3,14 @@ resource "azurerm_synapse_spark_pool" "pools" {
 
   synapse_workspace_id = azurerm_synapse_workspace.main.id
 
-  name                           = each.value.name
-  node_size_family               = each.value.node_size_family
-  node_size                      = each.value.node_size
-  node_count                     = each.value.node_count
-  cache_size                     = each.value.cache_size
-  spark_version                  = each.value.spark_version
-  session_level_packages_enabled = each.value.session_level_packages_enabled
+  name                                = each.value.name
+  node_size_family                    = each.value.node_size_family
+  node_size                           = each.value.node_size
+  node_count                          = each.value.node_count
+  cache_size                          = each.value.cache_size
+  spark_version                       = each.value.spark_version
+  session_level_packages_enabled      = each.value.session_level_packages_enabled
+  dynamic_executor_allocation_enabled = each.value.dynamic_executor_allocation_enabled
 
   auto_scale {
     max_node_count = each.value.auto_scale.max_node_count
